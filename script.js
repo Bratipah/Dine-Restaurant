5e1fc4a1c61c65d0e74b97b41591e1c983da6d9a
Bratipah <bratipahmh@gmail.com>
2021-09-01 15:25:46 -0700

    add navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle navbar
+const navToggler = document.querySelector(".nav-toggler")
+navToggler.addEventListener("click",toggleNav)
+
+function toggleNav(){
+    navToggler.classList.toggle("active")
+    document.querySelector(".nav").classList.toggle("open")
+}
